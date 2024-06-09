d665c576726cea92e5973bdcc00ae939efd406b2
mcamargorj <mcamargorj@gmail.com>
2024-06-09 09:40:28 -0300

    adição de colunas para delimitar área, tentativa de adaptação para celulares

--- app.py
+++ app.py
@@ -103,118 +103,120 @@ else:
     st.stop()
 
 # Header e descrição
-st.header('BACEN: Empresa x Quantidade de Reclamações por Tipo')
-fAdms = st.selectbox("Selecione a Empresa:", options=df_csv[coluna_empresa].unique())
+col = st.columns((0.1, 4.5, 0.1), gap='medium')
+with col[1]:
+    st.header('BACEN: Empresa x Quantidade de Reclamações por Tipo')
+    fAdms = st.selectbox("Selecione a Empresa:", options=df_csv[coluna_empresa].unique())
 
-dadosUsuario = df_csv[df_csv[coluna_empresa] == fAdms]
+    dadosUsuario = df_csv[df_csv[coluna_empresa] == fAdms]
 
-st.markdown(f'**Empresa**: {fAdms}')
+    st.markdown(f'**Empresa**: {fAdms}')
 
-# Ajustar os dados para o gráfico
-dadosUsuario = dadosUsuario.melt(id_vars=[coluna_empresa], value_vars=['Quantidade de reclamações reguladas procedentes', 'Quantidade de reclamações reguladas - outras', 'Quantidade de reclamações não reguladas'], var_name='Tipo de Reclamação', value_name='Quantidade')
+    # Ajustar os dados para o gráfico
+    dadosUsuario = dadosUsuario.melt(id_vars=[coluna_empresa], value_vars=['Quantidade de reclamações reguladas procedentes', 'Quantidade de reclamações reguladas - outras', 'Quantidade de reclamações não reguladas'], var_name='Tipo de Reclamação', value_name='Quantidade')
 
-# Reduzir o nome das legendas do gráfico
-dadosUsuario['Tipo de Reclamação'] = dadosUsuario['Tipo de Reclamação'].replace({
-    'Quantidade de reclamações reguladas procedentes': 'Reguladas Procedentes',
-    'Quantidade de reclamações reguladas - outras': 'Reguladas Outras',
-    'Quantidade de reclamações não reguladas': 'Não Reguladas'
-})
-
-
-# Suponha que `dadosUsuario` seja o DataFrame contendo seus dados
-
-grafCombEstado = alt.Chart(dadosUsuario).mark_bar().encode(
-    x=alt.X('Tipo de Reclamação:N', title='Tipo de Reclamação', axis=alt.Axis(labelAngle=-45)),
-    y=alt.Y('Quantidade:Q', title='Quantidade'),
-    color=alt.Color('Tipo de Reclamação:N', 
-                    scale=alt.Scale(range=['#00aca8', '#1d2262', '#d4096a']))
-).properties(
-    height=400,
-    width=800
-)
-
-# Adicionando os valores numéricos nas barras
-text = grafCombEstado.mark_text(
-    align='center',
-    baseline='middle',
-    dx=0,  # Nudge the text to the right so it doesn't appear on top of the bar
-    dy=-5  # Nudge the text upward for better alignment
-).encode(
-    text='Quantidade:Q'  # Use the 'Quantidade' column for the text
-)
-
-grafCombEstado = (grafCombEstado + text)
-
-st.altair_chart(grafCombEstado)
-
-# Início Tabela
-
-st.markdown('<h2 style="font-size: 26px;">Ranking de Reclamações</h2>', unsafe_allow_html=True)
-
-# Tratamento dos dados da coluna índice e rank
-
-# Remover pontos (separadores de milhar) e substituir vírgulas por pontos (separadores decimais).
-df_csv['Índice'] = df_csv['Índice'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
-# Converter a coluna para numérico.
-df_csv['Índice'] = pd.to_numeric(df_csv['Índice'], errors='coerce')
-# Ordenar a coluna do maior para o menor.
-df_ranking = df_csv.sort_values(by='Índice', ascending=False)
-# Selecionar apenas as 10 primeiras linhas, resetar o índice.
-df_ranking_top_10 = df_ranking.head(10).reset_index(drop=True)
-# Criar a coluna "Rank" com o formato "1º", "2º", etc.
-df_ranking_top_10['Rank'] = [f"{i+1}º" for i in df_ranking_top_10.index]
-# Reordenar as colunas para que "Rank" seja a primeira coluna.
-cols = ['Rank'] + [col for col in df_ranking_top_10.columns if col != 'Rank']
-df_ranking_top_10 = df_ranking_top_10[cols]
-# Formatar a coluna 'Índice' para exibir dois números após a vírgula.
-df_ranking_top_10['Índice'] = df_ranking_top_10['Índice'].map(lambda x: f"{x:.2f}")
-
-# Renomear as colunas
-df_ranking_top_10 = df_ranking_top_10.rename(columns={
-    'Instituição financeira': 'Instituição Financeira',
-    'Administradora de consórcio': 'Administradora de Consórcio',
-    'Índice': 'Índice <span style="cursor: pointer;" title="Número de reclamações dividido pelo número de consorciados ativos e multiplicado por 1.000.000">ℹ️</span>', 
-    'Quantidade de reclamações reguladas procedentes': 'Reguladas Procedentes',
-    'Quantidade de reclamações reguladas - outras': 'Reguladas Outras',
-    'Quantidade de reclamações não reguladas': 'Não Reguladas',
-    'Quantidade total de reclamações': 'Total'
-})
+    # Reduzir o nome das legendas do gráfico
+    dadosUsuario['Tipo de Reclamação'] = dadosUsuario['Tipo de Reclamação'].replace({
+        'Quantidade de reclamações reguladas procedentes': 'Reguladas Procedentes',
+        'Quantidade de reclamações reguladas - outras': 'Reguladas Outras',
+        'Quantidade de reclamações não reguladas': 'Não Reguladas'
+    })
 
 
-# Definindo o estilo da tabela modelo3
-styled_df = df_ranking_top_10.style.set_table_styles([
-    {'selector': 'thead th', 'props': [('font-size', '12pt'), ('font-weight', 'bold'), ('text-align', 'center'), ('background-color', '#404040'), ('color', 'white')]},  # Cor cinza escuro no cabeçalho
-    {'selector': 'tbody td', 'props': [('font-size', '10pt'), ('text-align', 'center')]},
-    {'selector': 'td.col1', 'props': [('max-width', '1000px'), ('white-space', 'normal'), ('text-align', 'left')]},  # Propriedades da coluna 1
-    {'selector': 'td.col0', 'props': [('font-weight', 'bold'), ('text-align', 'center')]},  # Alinhamento centralizado para a coluna de rank
-    {'selector': 'tbody tr:nth-child(even)', 'props': [('background-color', '#F2F2F2')]},  # Cor cinza claro nas linhas pares
-    {'selector': 'tbody tr:nth-child(odd)', 'props': [('background-color', 'white')]},  # Cor branca nas linhas ímpares
-]).set_properties(**{'white-space': 'pre-wrap', 'text-overflow': 'ellipsis'})
+    # Suponha que `dadosUsuario` seja o DataFrame contendo seus dados
 
-# Adicionando efeito de hover nas linhas da tabela com CSS
-styled_df.set_table_attributes('style="border-collapse: collapse; border: 2px solid #D3D3D3; box-shadow: 5px 5px 5px #888888;" class="styled-table"')
+    grafCombEstado = alt.Chart(dadosUsuario).mark_bar().encode(
+        x=alt.X('Tipo de Reclamação:N', title='Tipo de Reclamação', axis=alt.Axis(labelAngle=-45)),
+        y=alt.Y('Quantidade:Q', title='Quantidade'),
+        color=alt.Color('Tipo de Reclamação:N', 
+                        scale=alt.Scale(range=['#00aca8', '#1d2262', '#d4096a']))
+    ).properties(
+        height=400,
+        width=800
+    )
 
-# CSS personalizado para o efeito de hover
-css = """
-<style>
-.styled-table tbody tr:hover {
-    background-color: #d8f5ff !important;
-}
-</style>
-"""
-# Adicionando o CSS ao HTML da tabela
-styled_df = styled_df.set_caption(css)
+    # Adicionando os valores numéricos nas barras
+    text = grafCombEstado.mark_text(
+        align='center',
+        baseline='middle',
+        dx=0,  # Nudge the text to the right so it doesn't appear on top of the bar
+        dy=-5  # Nudge the text upward for better alignment
+    ).encode(
+        text='Quantidade:Q'  # Use the 'Quantidade' column for the text
+    )
 
-# Exibir a tabela estilizada sem o índice
-st.markdown(styled_df.hide(axis='index').to_html(escape=False), unsafe_allow_html=True)
+    grafCombEstado = (grafCombEstado + text)
 
-# Adicionar uma quebra de linha
-st.markdown("")  
+    st.altair_chart(grafCombEstado)
 
-# Adicionar um botão de download para o CSV
-st.download_button(
-    label="Baixar CSV",
-    data=df_ranking_top_10.to_csv(index=False).encode('utf-8'),
-    file_name='df_ranking_top_10.csv',
-    mime='text/csv'
-)
+    # Início Tabela
+
+    st.markdown('<h2 style="font-size: 26px;">Ranking de Reclamações</h2>', unsafe_allow_html=True)
+
+    # Tratamento dos dados da coluna índice e rank
+
+    # Remover pontos (separadores de milhar) e substituir vírgulas por pontos (separadores decimais).
+    df_csv['Índice'] = df_csv['Índice'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
+    # Converter a coluna para numérico.
+    df_csv['Índice'] = pd.to_numeric(df_csv['Índice'], errors='coerce')
+    # Ordenar a coluna do maior para o menor.
+    df_ranking = df_csv.sort_values(by='Índice', ascending=False)
+    # Selecionar apenas as 10 primeiras linhas, resetar o índice.
+    df_ranking_top_10 = df_ranking.head(10).reset_index(drop=True)
+    # Criar a coluna "Rank" com o formato "1º", "2º", etc.
+    df_ranking_top_10['Rank'] = [f"{i+1}º" for i in df_ranking_top_10.index]
+    # Reordenar as colunas para que "Rank" seja a primeira coluna.
+    cols = ['Rank'] + [col for col in df_ranking_top_10.columns if col != 'Rank']
+    df_ranking_top_10 = df_ranking_top_10[cols]
+    # Formatar a coluna 'Índice' para exibir dois números após a vírgula.
+    df_ranking_top_10['Índice'] = df_ranking_top_10['Índice'].map(lambda x: f"{x:.2f}")
+
+    # Renomear as colunas
+    df_ranking_top_10 = df_ranking_top_10.rename(columns={
+        'Instituição financeira': 'Instituição Financeira',
+        'Administradora de consórcio': 'Administradora de Consórcio',
+        'Índice': 'Índice <span style="cursor: pointer;" title="Número de reclamações dividido pelo número de consorciados ativos e multiplicado por 1.000.000">ℹ️</span>', 
+        'Quantidade de reclamações reguladas procedentes': 'Reguladas Procedentes',
+        'Quantidade de reclamações reguladas - outras': 'Reguladas Outras',
+        'Quantidade de reclamações não reguladas': 'Não Reguladas',
+        'Quantidade total de reclamações': 'Total'
+    })
+
+
+    # Definindo o estilo da tabela modelo3
+    styled_df = df_ranking_top_10.style.set_table_styles([
+        {'selector': 'thead th', 'props': [('font-size', '12pt'), ('font-weight', 'bold'), ('text-align', 'center'), ('background-color', '#404040'), ('color', 'white')]},  # Cor cinza escuro no cabeçalho
+        {'selector': 'tbody td', 'props': [('font-size', '10pt'), ('text-align', 'center')]},
+        {'selector': 'td.col1', 'props': [('max-width', '1000px'), ('white-space', 'normal'), ('text-align', 'left')]},  # Propriedades da coluna 1
+        {'selector': 'td.col0', 'props': [('font-weight', 'bold'), ('text-align', 'center')]},  # Alinhamento centralizado para a coluna de rank
+        {'selector': 'tbody tr:nth-child(even)', 'props': [('background-color', '#F2F2F2')]},  # Cor cinza claro nas linhas pares
+        {'selector': 'tbody tr:nth-child(odd)', 'props': [('background-color', 'white')]},  # Cor branca nas linhas ímpares
+    ]).set_properties(**{'white-space': 'pre-wrap', 'text-overflow': 'ellipsis'})
+
+    # Adicionando efeito de hover nas linhas da tabela com CSS
+    styled_df.set_table_attributes('style="border-collapse: collapse; border: 2px solid #D3D3D3; box-shadow: 5px 5px 5px #888888;" class="styled-table"')
+
+    # CSS personalizado para o efeito de hover
+    css = """
+    <style>
+    .styled-table tbody tr:hover {
+        background-color: #d8f5ff !important;
+    }
+    </style>
+    """
+    # Adicionando o CSS ao HTML da tabela
+    styled_df = styled_df.set_caption(css)
+
+    # Exibir a tabela estilizada sem o índice
+    st.markdown(styled_df.hide(axis='index').to_html(escape=False), unsafe_allow_html=True)
+
+    # Adicionar uma quebra de linha
+    st.markdown("")  
+
+    # Adicionar um botão de download para o CSV
+    st.download_button(
+        label="Baixar CSV",
+        data=df_ranking_top_10.to_csv(index=False).encode('utf-8'),
+        file_name='df_ranking_top_10.csv',
+        mime='text/csv'
+    )
