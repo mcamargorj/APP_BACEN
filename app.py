2d8f13603d5b1488be2854136fc452af09ae8609
Marcelo Camargo <167449455+mcamargorj@users.noreply.github.com>
2024-06-09 20:37:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ else:
 
 # Header e descrição
 col = st.columns((7.0, 0.1), gap='medium')
-with col[1]:
+with col[0]:
     st.header('BACEN: Empresa x Quantidade de Reclamações por Tipo')
     fAdms = st.selectbox("Selecione a Empresa:", options=df_csv[coluna_empresa].unique())
 
