45c583b88c5c250a6edcaa2bab2fbf0bb0b0227e
Marcelo Camargo <167449455+mcamargorj@users.noreply.github.com>
2025-02-03 08:41:14 -0300

    Update app.py
    Atualização do index para pegar o 2 semestre de 2024.

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ with st.sidebar:
     # Criando menu para seleção
     tipo_dropdown = st.selectbox('Selecione o tipo:', options=tipos_unicos, index=1)
     ano_dropdown = st.selectbox('Selecione o ano:', options=df[df['tipo'] == tipo_dropdown]['ano'].unique().tolist(), index=10)
-    periodicidade_dropdown = st.selectbox('Selecione a periodicidade:', options=df[(df['tipo'] == tipo_dropdown) & (df['ano'] == ano_dropdown)]['periodicidade'].unique().tolist())
+    periodicidade_dropdown = st.selectbox('Selecione a periodicidade:', options=df[(df['tipo'] == tipo_dropdown) & (df['ano'] == ano_dropdown)]['periodicidade'].unique().tolist(), index=1)
     periodo_dropdown = st.selectbox('Selecione o período:', options=df[(df['tipo'] == tipo_dropdown) & (df['ano'] == ano_dropdown) & (df['periodicidade'] == periodicidade_dropdown)]['periodo'].unique().tolist())
 
 # Gerar o link do CSV com base nos valores selecionados
